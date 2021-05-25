04b4d42c77d1609d19bab3317475ed493070ac92
NathanNeelis <nathan.neelis@hva.nl>
2021-05-25 16:44:30 +0200

    Working on transforming the data

--- routes/home.js
+++ routes/home.js
@@ -5,33 +5,59 @@ const getData = require('../utils/getData');
 // export
 async function home(req, res) {
 
-    // const endpoint = 'https://test2.hipperacademy.nl/index.php/clients/'
-    const clientID = '1096' // open to change
-    const pam = '/pam'
-    const daily = '/daily'
-    const raw = '/raw'
-    const dateStart = '/2017-12-20'
-    const dateEnd = '/2018-1-1'
+    // daily PAM values
+    const dailyPAMURL = 'https://gist.githubusercontent.com/NathanNeelis/f83a294032223066bdddbd5ff37c9dc7/raw/4853849d9da46786b557a131937759bb16218dbc/pam_today'
 
-    const endpoint = 'https://cors-anywhere.herokuapp.com/https://test2.hipperacademy.nl/index.php/clients/1096/pam/daily/2017-12-20/2018-1-1';
+    // EPOCH VALUES - PAM each 15 minutes
+    const epochURL = 'https://gist.githubusercontent.com/NathanNeelis/9a449e389ab30351369c8556ce634b1b/raw/38da52499b41c51af294845d8576c18a85964aff/epoch_values'
 
+    const weeklyData = await getData(dailyPAMURL) // 2 weekly data split in days
+    const rawDailyData = await getData(epochURL) // data every 15 minutes
 
-    const dailyURL = 'https://test2.hipperacademy.nl/index.php/clients/1096/pam/daily/2017-12-20/2018-1-1'
+    const currentDay = weeklyData[14] // This should be automated for the current day.
+    const currentWeek = getCurrentWeek(weeklyData)
 
-    // const rawData = await getData(endpoint + clientID + pam + raw + dateStart + dateEnd); // FETCH THE RAW DATA
-    // const dailyData = await getData(endpoint + clientID + pam + daily + dateStart + dateEnd); // FETCH THE DAILY DATA
-    // const data = await getData(endpoint)
+    renderPage(weeklyData, rawDailyData, currentDay)
 
-    // console.log('rawData', rawData);
-    // console.log('dailyData', dailyData);
-    // console.log('data', data);
-
-    res.render("home.ejs", {
-        // rawData: rawData,
-        // dailyData: dailyData
-    });
+    function renderPage(weeklyData, rawDailyData, currentDay) {
+        // console.log("this is weekly data", weeklyData);
+        // console.log("this is raw daily data", rawDailyData);
+        // console.log(currentDay)
+        res.render("home.ejs", {
+            weeklyData: weeklyData,
+            rawDailyData: rawDailyData,
+            currentDay: currentDay
+        });
+    }
 
 }
 
 
 module.exports = home;
+
+
+
+function getCurrentDay(data) {
+    console.log(data.length, data[14].values[0].pam)
+}
+
+
+function getCurrentWeek(data) {
+    let arrayPam = data.map((data, index) => {
+        return {
+            pam: parseInt(data.values[0].pam, 10),
+        }
+    });
+
+    let arrayTotal = 0;
+    let length = arrayPam.length;
+    arrayPam.forEach(({
+        pam
+    }) => arrayTotal += pam);
+
+    const avarage = Math.round(arrayTotal / length);
+    console.log('avarage pam score dataset', avarage)
+
+    return avarage
+
+}
