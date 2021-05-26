e702848ffe698b1e94ae5b23b2ab7e4c21af6d12
NathanNeelis <nathan.neelis@hva.nl>
2021-05-26 13:56:18 +0200

    added main page, manifest and service worker

--- routes/home.js
+++ routes/home.js
@@ -43,12 +43,15 @@ function getCurrentDay(data) {
 
 
 function getCurrentWeek(data) {
+    // new array with object only containing the pam value
     let arrayPam = data.map((data, index) => {
         return {
-            pam: parseInt(data.values[0].pam, 10),
+            pam: parseInt(data.values[0].pam, 10), // string to integer
         }
     });
 
+    // getting the avarage pam score
+    // Resource: https://stackoverflow.com/questions/52513123/how-to-get-the-average-from-array-of-objects
     let arrayTotal = 0;
     let length = arrayPam.length;
     arrayPam.forEach(({
