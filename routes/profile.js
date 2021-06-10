6e23cd375aeb1f22c31414bc51747d60970d8662
NathanNeelis <nathan.neelis@hva.nl>
2021-06-10 22:05:29 +0200

    info and profile page basics

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,22 @@
-const info = (req, res) => {
-    if (req.session.user) {
-        res.render('profile.ejs', {
-            user: req.session.user.user
-        });
-    } else {
-        res.render('login.ejs', {
-            error: false,
-        })
-    }
-};
+ // Utils
+ const getData = require('../utils/getData');
+ const calcDefaultGoal = require('../utils/calcDefaultGoal')
 
-module.exports = info;
+ const info = async (req, res) => {
+     const dailyPAMURL = 'https://gist.githubusercontent.com/NathanNeelis/f83a294032223066bdddbd5ff37c9dc7/raw/4853849d9da46786b557a131937759bb16218dbc/pam_today'
+     const weeklyData = await getData(dailyPAMURL)
+     const currentWeek = calcDefaultGoal(weeklyData) // At the moment the PAM average of all the data plus 1
+
+     if (req.session.user) {
+         res.render('profile.ejs', {
+             user: req.session.user.user,
+             currentWeek: currentWeek,
+         });
+     } else {
+         res.render('login.ejs', {
+             error: false,
+         })
+     }
+ };
+
+ module.exports = info;
